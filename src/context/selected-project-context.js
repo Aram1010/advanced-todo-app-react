eed983463d8ecef58d93b32c38b24dc4bea14a57
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-18 23:04:47 +0400

    selected project

---
+++ src/context/selected-project-context.js
@@ -0,0 +1,16 @@
+import React, { createContext, useContext, useState } from "react";
+import { useProjects } from "../hooks";
+
+export const SelectedProjectContext = createContext();
+
+export const SelectedProjectProvider = ({ children }) => {
+  const [selectedProject, setSelectedProject ] = useState("Inbox");
+
+  return (
+    <SelectedProjectContext.Provider value={{ selectedProject, setSelectedProject }}>
+      {children}
+    </SelectedProjectContext.Provider>
+  );
+};
+
+export const useSelectedProjectValue = () => useContext(SelectedProjectContext);
