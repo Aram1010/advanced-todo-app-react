511352116ba8c708bf89c41f8f85f950df3c28b6
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-21 08:37:59 +0400

    little changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Login, Home } from "./components/layout/index";
+import { ProjectsProvider, SelectedProjectProvider } from "./context";
 
 const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/tasks" element={<Home />} />
-      </Routes>
-    </div>
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/tasks" element={<Home />} />
+          </Routes>
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
   );
 };
 
