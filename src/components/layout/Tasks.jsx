8425749957f7052d4de8e77d78ac15138b36b4ed
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-23 08:07:15 +0400

    added deleting task functionality

--- src/components/layout/Tasks.jsx
+++ src/components/layout/Tasks.jsx
@@ -4,10 +4,17 @@ import { useTasks } from "../../hooks/index";
 import { useSelectedProjectValue } from "../../context";
 import AddTask from "../AddTask";
 import Checkbox from "../Checkbox";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
 const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
   const { tasks } = useTasks(selectedProject);
+
+  const deleteTask = (taskId) => {
+    const docRef = doc(db, "tasks", taskId);
+    deleteDoc(docRef)
+  }
 
   return (
     <div>
@@ -18,18 +25,23 @@ const Tasks = () => {
             data-id-key={task.id}
             key={task.id}
             className="flex items-center my-[10px]"
-            style={
-              task.isCompleted
-                ? { textDecoration: "line-through", color: "#AEAEAE" }
-                : null
-            }
           >
             <Checkbox
               taskId={task.id}
               taskName={task.task}
               taskIsCompleted={task.isCompleted}
             />
-            <span className="ml-[10px]">{task.task}</span>
+            <span
+              style={
+                task.isCompleted
+                  ? { textDecoration: "line-through", color: "#AEAEAE" }
+                  : null
+              }
+              className="ml-[10px]"
+            >
+              {task.task}
+            </span>
+            <button className="ml-[20px]" onClick={() => deleteTask(task.id)}>delete task</button>
           </li>
         ))}
       <AddTask />
